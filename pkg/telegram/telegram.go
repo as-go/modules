eb9bf0b80575e1e1bc7353c3154f8b3851ab486a
Alexander Churkin <alex.asutp@gmail.com>
2022-07-01 17:25:28 +0300

    Fix commit

--- pkg/telegram/telegram.go
+++ pkg/telegram/telegram.go
@@ -5,5 +5,5 @@ import "fmt"
 // New create the telegram client
 func New() {
 	fmt.Println("I'm println from telegram module!!!")
-	fmt.Println("I'm println from telegram module!!!")
+	fmt.Println("Fixed version")
 }
